03078877aeb23fb9a076aea69c9b1bb2cb969298
James Anderson <james.p.anderson16@gmail.com>
2017-01-25 09:00:09 -0700

    robot first commit

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,43 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
-  get '/robots' do
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/robots' do #see all robots
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect '/robots'
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.delete(id.to_i)
+    redirect '/robots'
+  end
+
+end
